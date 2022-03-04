12c5c6f32a6f241471a1182bed4624f25ed4e45d
sefaenestas <94571116+muveso@users.noreply.github.com>
2022-03-04 12:16:56 +0300

    First Release

---
+++ CameraAlignment/Assets/CameraAlignment/CameraHolder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+#endif
+
+namespace CameraAlignment
+{
+    public class CameraHolder : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [SerializeField] public bool EnableAlignment { get; set; }
+#endif
+    }
+}
---
+++ CameraAlignment/Assets/CameraAlignment/Editor/CameraHolderEditor.cs
@@ -0,0 +1,86 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CameraAlignment.Editor
+{
+    [CustomEditor(typeof(CameraHolder))]
+    public class CameraHolderEditor : UnityEditor.Editor
+    {
+        
+        private static Camera _camera;
+        private static CameraHolder _cameraHolder;
+        private static bool _subscribedSceneGUI;
+        
+        private const string EnableText = "Enable Camera Alignment";
+        private const string DisableText = "Disable Camera Alignment";
+
+        private void OnEnable()
+        {
+            if (_subscribedSceneGUI) return;
+            
+            _subscribedSceneGUI = true;
+            SceneView.duringSceneGui += CameraUpdate;
+        }
+        
+        public override void OnInspectorGUI()
+        {
+            _cameraHolder = (CameraHolder) target;
+        }
+        
+        private static void CameraUpdate(SceneView sceneView)
+        {
+            if (!_cameraHolder) return;
+            DrawButton();
+            UpdateTransform(sceneView);
+        }
+
+        private static void UpdateTransform(SceneView sceneView)
+        {
+            if (!_cameraHolder.EnableAlignment) return;
+            var gameTransform = _camera.transform;
+            var sceneTransform = sceneView.camera.transform;
+
+            gameTransform.position = sceneTransform.position;
+            gameTransform.rotation = sceneTransform.rotation;
+        }
+
+        private static void DrawButton()
+        {
+            Handles.BeginGUI();
+            var oldColor = GUI.color;
+
+            if (!_cameraHolder.EnableAlignment)
+            {
+                GUI.color = Color.green;
+                if (GUILayout.Button(EnableText, GUILayout.Width(200), GUILayout.Height(40)))
+                {
+                    DisableAllScripts();
+
+                    _camera = _cameraHolder.gameObject.GetComponent<Camera>();
+                    _cameraHolder.EnableAlignment = true;
+                }
+            }
+            else
+            {
+                GUI.color = Color.red;
+                if (GUILayout.Button(DisableText, GUILayout.Width(200), GUILayout.Height(40)))
+                {
+                    _camera = null;
+                    _cameraHolder.EnableAlignment = false;
+                }
+            }
+
+            GUI.color = oldColor;
+            EditorUtility.SetDirty(_cameraHolder.gameObject);
+            Handles.EndGUI();
+        }
+
+        private static void DisableAllScripts()
+        {
+            var attachedCameras = FindObjectsOfType<CameraHolder>();
+            foreach (var cam in attachedCameras)
+                cam.EnableAlignment = false;
+        }
+
+    }
+}
